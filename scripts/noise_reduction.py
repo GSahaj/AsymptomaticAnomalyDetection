d1b7636c43e2eba7dcbe6af2a125e4f71d91cb4d
Gursahaj Chawla <sahaj06chawla@gmail.com>
2025-03-30 14:16:34 -0400

    Delete scripts directory

--- scripts/noise_reduction.py
+++
@@ -1,9 +0,0 @@
-import noisereduce as nr
-import librosa
-
-def reduce_noise(audio, sr=160000):
-    #Reduce background noise from the audio signal
-
-    #Apply noise reducion
-    clean_audio = nr.reduce_noise(y=audio, sr=sr)
-    return clean_audio
